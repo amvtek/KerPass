f0cdf0ec220c1ee24543043b99404d3a20fc1a8b
amvtek <devel@amvtek.com>
2025-04-03 16:35:34 +0300

    Add noise protocol CipherState implementation.

--- pkg/noise/cipher.go
+++ pkg/noise/cipher.go
@@ -8,57 +8,129 @@ import (
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
-type cipherKey int
+const (
+	MAX_UINT64       = 0xFFFF_FFFF_FFFF_FFFF
+	CIPHER_MAX_NONCE = MAX_UINT64 - 1
+)
+
+type cipherRef int
 
 const (
-	CIPHER_AES256_GCM cipherKey = iota
+	CIPHER_AES256_GCM cipherRef = iota
 	CIPHER_CHACHA20_POLY1305
 )
 
-func GetCipher(ck cipherKey) (Cipher, error) {
-	switch ck {
+type CipherState struct {
+	factory aeadFactory
+	aead    aeadIfce
+	k       [32]byte
+	n       uint64
+	nonce   [12]byte
+}
+
+func NewCipherState(algo cipherRef) (*CipherState, error) {
+
+	var newAead aeadFactory
+	switch algo {
 	case CIPHER_AES256_GCM:
-		return aes256GCM{}, nil
+		newAead = newAESGCM
 	case CIPHER_CHACHA20_POLY1305:
-		return chacha{}, nil
+		newAead = newChachaPoly1305
 	default:
 		return nil, ErrUnsupportedCipher
 	}
+	return &CipherState{factory: newAead}, nil
 }
 
-type Cipher interface {
-	Encrypt(key []byte, nonce uint64, ad []byte, plaintext []byte) ([]byte, error)
-	Decrypt(key []byte, nonce uint64, ad []byte, ciphertext []byte) ([]byte, error)
+func (self *CipherState) HasKey() bool {
+	return (nil != self.aead)
 }
 
-type aes256GCM struct{}
-
-func (self aes256GCM) Encrypt(key []byte, nonce uint64, ad []byte, plaintext []byte) ([]byte, error) {
-	aead, err := self.getCipher(key)
-	if nil != err {
-		return nil, err
+func (self *CipherState) InitializeKey(newkey []byte) error {
+	numbytes := copy(self.key(), newkey)
+	if numbytes < 32 {
+		return ErrRekeyLowEntropy
 	}
+	aead, err := self.factory(self.key())
+	if nil != err {
+		return err
+	}
+	self.aead = aead
+	self.n = 0
+	return nil
+}
 
-	nonce96 := make([]byte, 12)
-	self.fillNonceBuffer(nonce96, nonce)
+func (self *CipherState) SetNonce(n uint64) {
+	self.n = n
+}
 
-	ciphertext := aead.Seal(nil, nonce96, plaintext, ad)
+func (self *CipherState) EncryptWithAd(ad, plaintext []byte) ([]byte, error) {
+	if !self.HasKey() {
+		return plaintext, nil
+	}
+	if CIPHER_MAX_NONCE == self.n {
+		return nil, ErrCipherKeyOverUse
+	}
+	nonce := self.nonce[:]
+	self.aead.fillNonce(nonce, self.n)
+	ciphertext := self.aead.Seal(nil, nonce, plaintext, ad)
+	self.n += 1
 	return ciphertext, nil
 }
 
-func (self aes256GCM) Decrypt(key []byte, nonce uint64, ad []byte, ciphertext []byte) ([]byte, error) {
-	aead, err := self.getCipher(key)
+func (self *CipherState) DecryptWithAd(ad, ciphertext []byte) ([]byte, error) {
+	if !self.HasKey() {
+		return ciphertext, nil
+	}
+	if CIPHER_MAX_NONCE == self.n {
+		return nil, ErrCipherKeyOverUse
+	}
+	nonce := self.nonce[:]
+	self.aead.fillNonce(nonce, self.n)
+	plaintext, err := self.aead.Open(nil, nonce, ciphertext, ad)
 	if nil != err {
 		return nil, err
 	}
-
-	nonce96 := make([]byte, 12)
-	self.fillNonceBuffer(nonce96, nonce)
-
-	return aead.Open(nil, nonce96, ciphertext, ad)
+	self.n += 1 // spec says not to increment if Decrypt fails
+	return plaintext, nil
 }
 
-func (_ aes256GCM) getCipher(key []byte) (cipher.AEAD, error) {
+func (self *CipherState) Rekey() error {
+	if !self.HasKey() {
+		return ErrInvalidCipherState
+	}
+	newkey := self.k[:]
+	err := self.aead.rekey(newkey, self.nonce[:])
+	if nil != err {
+		return err
+	}
+	aead, err := self.factory(newkey)
+	if nil != err {
+		return err
+	}
+	self.aead = aead
+	// RMQ: we keep n counter inchanged as the spec says nothing about it.
+	return nil
+}
+
+func (self *CipherState) key() []byte {
+	return self.k[:]
+}
+
+// aeadIfce extends cipher.AEAD with methods usefull for noise protocol implementation.
+type aeadIfce interface {
+	cipher.AEAD
+	rekey(newkey []byte, nonce []byte) error
+	fillNonce(nonce []byte, n uint64)
+}
+
+type aeadFactory func([]byte) (aeadIfce, error)
+
+type aesGCMAEAD struct {
+	cipher.AEAD
+}
+
+func newAESGCM(key []byte) (aeadIfce, error) {
 	if len(key) != 32 {
 		return nil, ErrInvalidCipherKeySize
 	}
@@ -67,56 +139,58 @@ func (_ aes256GCM) getCipher(key []byte) (cipher.AEAD, error) {
 	if nil != err {
 		return nil, err
 	}
-	return cipher.NewGCM(block)
+	aead, err := cipher.NewGCM(block)
+	if nil != err {
+		return nil, err
+	}
+	return aesGCMAEAD{AEAD: aead}, nil
+
 }
 
-func (_ aes256GCM) fillNonceBuffer(dst []byte, value uint64) {
-	if len(dst) < 12 {
+func (self aesGCMAEAD) rekey(newkey []byte, nonce []byte) error {
+	self.fillNonce(nonce, CIPHER_MAX_NONCE)
+	zeros := make([]byte, 64)
+	ciphertext := self.Seal(nil, nonce, zeros[:32], nil)
+	numcopied := copy(newkey, ciphertext)
+	if numcopied < 32 {
+		return ErrRekeyLowEntropy
+	}
+	copy(ciphertext, zeros)
+	return nil
+}
+
+func (_ aesGCMAEAD) fillNonce(nonce []byte, n uint64) {
+	if len(nonce) < 12 {
+		// if nonce does not have the correct size, this is an implementation error
 		panic("Invalid nonce buffer size")
 	}
-	binary.BigEndian.PutUint32(dst, 0)
-	binary.BigEndian.PutUint64(dst[4:], value)
+	binary.BigEndian.PutUint32(nonce, 0)
+	binary.BigEndian.PutUint64(nonce[4:], n)
 }
 
-type chacha struct{}
-
-func (self chacha) Encrypt(key []byte, nonce uint64, ad []byte, plaintext []byte) ([]byte, error) {
-	aead, err := self.getCipher(key)
-	if nil != err {
-		return nil, err
-	}
-
-	nonce96 := make([]byte, 12)
-	self.fillNonceBuffer(nonce96, nonce)
-
-	ciphertext := aead.Seal(nil, nonce96, plaintext, ad)
-	return ciphertext, nil
+type chachaPoly1305AEAD struct {
+	aesGCMAEAD
 }
 
-func (self chacha) Decrypt(key []byte, nonce uint64, ad []byte, ciphertext []byte) ([]byte, error) {
-	aead, err := self.getCipher(key)
-	if nil != err {
-		return nil, err
-	}
-
-	nonce96 := make([]byte, 12)
-	self.fillNonceBuffer(nonce96, nonce)
-
-	return aead.Open(nil, nonce96, ciphertext, ad)
-}
-
-func (_ chacha) getCipher(key []byte) (cipher.AEAD, error) {
+func newChachaPoly1305(key []byte) (aeadIfce, error) {
 	if len(key) != 32 {
 		return nil, ErrInvalidCipherKeySize
 	}
 
-	return chacha20poly1305.New(key)
+	aead, err := chacha20poly1305.New(key)
+	if nil != err {
+		return nil, err
+	}
+	rv := chachaPoly1305AEAD{}
+	rv.AEAD = aead
+	return rv, nil
 }
 
-func (_ chacha) fillNonceBuffer(dst []byte, value uint64) {
-	if len(dst) < 12 {
+func (_ chachaPoly1305AEAD) fillNonce(nonce []byte, n uint64) {
+	if len(nonce) < 12 {
+		// if nonce does not have the correct size, this is an implementation error
 		panic("Invalid nonce buffer size")
 	}
-	binary.LittleEndian.PutUint32(dst, 0)
-	binary.LittleEndian.PutUint64(dst[4:], value)
+	binary.LittleEndian.PutUint32(nonce, 0)
+	binary.LittleEndian.PutUint64(nonce[4:], n)
 }
