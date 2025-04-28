f84517c25bb3dd8cf6d7e4d5846d6c4e8e906c8e
amvtek <devel@amvtek.com>
2025-04-28 12:05:34 +0300

    Add Error flag.

--- internal/utils/errors_test.go
+++ internal/utils/errors_test.go
@@ -9,8 +9,8 @@ import (
 func TestErrorFoo(t *testing.T) {
 	err := foo()
 	t.Logf("err -> %v", err)
-	if !errors.Is(err, PkgBaseError) {
-		t.Error("Oops, err is not PkgBaseError")
+	if !errors.Is(err, Error) {
+		t.Error("Oops, err is not Error")
 	}
 	_, ok := err.(RaisedErr)
 	if !ok {
@@ -21,8 +21,8 @@ func TestErrorFoo(t *testing.T) {
 func TestErrorBar(t *testing.T) {
 	err := bar()
 	t.Logf("err -> %v", err)
-	if !errors.Is(err, PkgBaseError) {
-		t.Error("Oops, err is not PkgBaseError")
+	if !errors.Is(err, Error) {
+		t.Error("Oops, err is not Error")
 	}
 	if !errors.Is(err, io.EOF) {
 		t.Error("Oops, err is not an io.EOF")
@@ -37,51 +37,18 @@ func TestErrorBaz(t *testing.T) {
 	errs := baz()
 	for pos, err := range errs {
 		t.Logf("#%d: err -> %v", pos, err)
-		if !errors.Is(err, PkgBaseError) {
+		if !errors.Is(err, Error) {
 			t.Errorf("#%d: err is not a noise.Error", pos)
 		}
 		_, ok := err.(RaisedErr)
 		if !ok {
-			t.Errorf("#%d: can not cast err to raisedErr", pos)
+			t.Errorf("#%d: can not cast err to RaisedErr", pos)
 		}
 	}
 	err := errs[1]
 	if !errors.Is(err, io.EOF) {
 		t.Error("Oops, err is not an io.EOF")
 	}
-}
-
-// ---
-// Below definitions show how WrappedError is intended to be used in practice.
-
-// first we define an error type for package error flags
-type errorFlag string
-
-// and then at least one global flag error constant
-const (
-	PkgBaseError = errorFlag("utils: error")
-	noError      = errorFlag("")
-)
-
-func (self errorFlag) Error() string {
-	return string(self)
-}
-
-func (self errorFlag) Unwrap() error {
-	if noError == self || PkgBaseError == self {
-		return nil
-	}
-	return PkgBaseError
-}
-
-// then we define newError & wrapError to be used for all package errors...
-
-func newError(msg string, args ...any) error {
-	return NewError(1, PkgBaseError, msg, args...)
-}
-
-func wrapError(cause error, msg string, args ...any) error {
-	return WrapError(cause, 1, PkgBaseError, msg, args...)
 }
 
 func foo() error {
