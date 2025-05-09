e6da0051171bc2996f4140d6866ee7668a075643
amvtek <devel@amvtek.com>
2025-05-09 17:19:10 +0300

    Add HKDF inputs to EPHEMSEC TestVector.

--- pkg/ephemsec/support.go
+++ pkg/ephemsec/support.go
@@ -13,7 +13,7 @@ type TestVector struct {
 	Context                  utils.HexBinary `json:"context"`
 	Psk                      utils.HexBinary `json:"psk"`
 	SharedSecret             utils.HexBinary `json:"shared_secret"`
-	Otp string `json:"otp"`
+	Otp                      string          `json:"otp"`
 	InitiatorNonce           utils.HexBinary `json:"init_nonce"`
 	InitiatorTime            int64           `json:"init_time"`
 	InitiatorStaticKey       utils.HexBinary `json:"init_static_key"`
@@ -26,6 +26,9 @@ type TestVector struct {
 	ResponderEphemKey        utils.HexBinary `json:"resp_ephemeral_key"`
 	ResponderRemoteStaticKey utils.HexBinary `json:"resp_remote_static_key"`
 	ResponderRemoteEphemKey  utils.HexBinary `json:"resp_remote_ephemeral_key"`
+	HkdfSalt                 utils.HexBinary `json:"hkdf_salt"`
+	HkdfInfo                 utils.HexBinary `json:"hkdf_info"`
+	HkdfSecret               utils.HexBinary `json:"hkdf_secret"`
 }
 
 // LoadTestVector loads test vectors from json file at srcpath.
@@ -35,9 +38,7 @@ func LoadTestVectors(srcpath string) ([]TestVector, error) {
 		return nil, wrapError(err, "failed opening file %s", srcpath)
 	}
 	dec := json.NewDecoder(src)
-	s1 := struct {
-		Vectors []TestVector `json:"vectors"`
-	}{}
-	err = dec.Decode(&s1)
-	return s1.Vectors, wrapError(err, "failed decoding json test vectors")
+	rv := []TestVector{}
+	err = dec.Decode(&rv)
+	return rv, wrapError(err, "failed decoding json test vectors")
 }
