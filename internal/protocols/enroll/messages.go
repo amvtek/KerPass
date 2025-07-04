23ac8588f10d574e6a0717833a989d40fb30e89f
amvtek <devel@amvtek.com>
2025-07-04 10:50:09 +0300

    Add package documentation.

--- internal/protocols/enroll/messages.go
+++ internal/protocols/enroll/messages.go
@@ -3,10 +3,12 @@ package enroll
 // EnrollReq is sent by the CardAgent client to the KerPass server.
 // It is a plaintext that starts the EnrollProtocol.
 type EnrollReq struct {
-	RealmId  []byte `json:"1" cbor:"1,keyasint"`
+	RealmId  []byte `json:"1" cbor:"1,keyasint"` // Determine the Static Key used by the Server
 	NoiseMsg []byte `json:"2" cbor:"2,keyasint"`
 }
 
+// EnrollCardCreateResp is sent by the KerPass server to the CardAgent client.
+// It contains information that are necessary for creating the Card.
 type EnrollCardCreateResp struct {
 	RealmId []byte `json:"1" cbor:"1,keyasint"`
 	CardId  []byte `json:"2" cbor:"2,keyasint"`
