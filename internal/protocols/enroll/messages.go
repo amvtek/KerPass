69c897c4e3e217b0da6e89f10e2691216521143d
amvtek <devel@amvtek.com>
2025-07-18 17:38:12 +0300

    Add Check methods to enroll protocol messages.

--- internal/protocols/enroll/messages.go
+++ internal/protocols/enroll/messages.go
@@ -3,15 +3,55 @@ package enroll
 // EnrollReq is sent by the CardAgent client to the KerPass server.
 // It is a plaintext that starts the EnrollProtocol.
 type EnrollReq struct {
-	RealmId  []byte `json:"1" cbor:"1,keyasint"` // Determine the Static Key used by the Server
-	NoiseMsg []byte `json:"2" cbor:"2,keyasint"`
+	RealmId []byte `json:"realm_id" cbor:"1,keyasint"` // Determine the Static Key used by the Server
+}
+
+func (self EnrollReq) Check() error {
+	rsz := len(self.RealmId)
+	if rsz < 32 || rsz > 64 {
+		return newError("invalid RealmId size, %d not in 32..64 range", rsz)
+	}
+
+	return nil
+}
+
+// EnrollAuthorization is sent by the CardAgent client to the KerPass server.
+type EnrollAuthorization struct {
+	AuthorizationId []byte `json:"authorization_id" cbor:"1,keyasint"`
+	PSKShare        []byte `json:"client_psk_share" cbor:"2,keyasint"`
+}
+
+func (self EnrollAuthorization) Check() error {
+	asz := len(self.AuthorizationId)
+	if asz < 32 {
+		return newError("invalid AuthorizationId size, %d < 32", asz)
+	}
+	psz := len(self.PSKShare)
+	if psz < 32 {
+		return newError("invalid PSKShare size, %d < 32", psz)
+	}
+
+	return nil
 }
 
 // EnrollCardCreateResp is sent by the KerPass server to the CardAgent client.
 // It contains information that are necessary for creating the Card.
 type EnrollCardCreateResp struct {
-	RealmId []byte `json:"1" cbor:"1,keyasint"`
-	CardId  []byte `json:"2" cbor:"2,keyasint"`
-	AppName string `json:"3" cbor:"3,keyasint"`
-	AppLogo []byte `json:"4" cbor:"4,keyasint"`
+	CardId   []byte `json:"card_id" cbor:"1,keyasint"`
+	PSKShare []byte `json:"psk_share" cbor:"2,keyasint"`
+	AppName  string `json:"app_name" cbor:"3,keyasint"`
+	AppLogo  []byte `json:"app_logo" cbor:"4,keyasint"`
+}
+
+func (self EnrollCardCreateResp) Check() error {
+	csz := len(self.CardId)
+	if csz < 32 {
+		return newError("Invalid CardId size, %d < 32", csz)
+	}
+	psz := len(self.PSKShare)
+	if psz < 32 {
+		return newError("invalid ServerPSKShare size, %d < 32", psz)
+	}
+
+	return nil
 }
