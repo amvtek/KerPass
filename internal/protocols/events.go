86fa14189ec21e8d3fa5678552da6ecff6e04159
amvtek <devel@amvtek.com>
2025-07-22 10:52:01 +0300

    Add Event type to hold incoming Protocol data.

---
+++ internal/protocols/events.go
@@ -0,0 +1,20 @@
+package protocols
+
+const (
+	EvtInit    = "Init"
+	EvtMsg     = "ReadMessage"
+	EvtAbort   = "Abort"
+	EvtTimeout = "Timeout"
+)
+
+// Event contains "incoming" data processed by a ProtocolFSM.
+type Event struct {
+	Tag  string `json:"tag" cbor:"1,keyasint"`
+	Msg  []byte `json:"msg,omitempty" cbor:"2,keyasint,omitempty"`
+	Data any    `json:"data,omitempty" cbor:"3,keyasint,omitempty"`
+}
+
+// Init returns an EvtInit Event used to initialize a ProtocolFSM.
+func Init() Event {
+	return Event{Tag: EvtInit}
+}
