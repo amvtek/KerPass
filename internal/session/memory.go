abd40d952a21eee2e2cf2ec5a895f9d534aef0fa
amvtek <devel@amvtek.com>
2025-09-19 18:56:07 +0300

    Add in memory session Store implementation.

---
+++ internal/session/memory.go
@@ -0,0 +1,132 @@
+package session
+
+import (
+	"sync"
+)
+
+const (
+	numSlot = 16
+)
+
+type TimedKey interface {
+	comparable
+	Timed
+}
+
+type slot[K TimedKey, V any] struct {
+	mut   sync.RWMutex
+	t     int64
+	store map[K]V
+}
+
+// MemStore is an in memory session Store that automatically expires keys.
+type MemStore[K TimedKey, V any] struct {
+	KeyFacto KeyFactory[K]
+	slots    [numSlot]slot[K, V]
+}
+
+// NewMemStore instantiates a new MemStore.
+// It errors if kf is nil.
+func NewMemStore[K TimedKey, V any](kf KeyFactory[K]) (*MemStore[K, V], error) {
+	if nil == kf {
+		return nil, newError("nil KeyFactory")
+	}
+
+	return &MemStore[K, V]{KeyFacto: kf}, nil
+}
+
+// Get returns the value indexed by key.
+// The bool flag is true if the key exists in the MemStore.
+func (self *MemStore[K, V]) Get(key K) (V, bool) {
+	var v V
+	var present bool
+
+	if err := self.KeyFacto.Check(key); nil != err {
+		return v, present
+	}
+
+	ts := key.T()
+	slot := &(self.slots[ts%numSlot])
+	slot.mut.RLock()
+	defer slot.mut.RUnlock()
+
+	if ts == slot.t {
+		v, present = slot.store[key]
+	}
+
+	return v, present
+
+}
+
+// Pop removes the key from the MemStore and returns the associated value.
+// The bool flag is true if the key was found in the MemStore.
+func (self *MemStore[K, V]) Pop(key K) (V, bool) {
+	var v V
+	var present bool
+
+	if err := self.KeyFacto.Check(key); nil != err {
+		return v, present
+	}
+
+	ts := key.T()
+	slot := &(self.slots[ts%numSlot])
+	slot.mut.Lock()
+	defer slot.mut.Unlock()
+
+	if ts == slot.t {
+		v, present = slot.store[key]
+		delete(slot.store, key)
+	}
+
+	return v, present
+
+}
+
+// Set registers key, data in the MemStore.
+// It errors if key is not valid.
+func (self *MemStore[K, V]) Set(key K, data V) error {
+	// validate key
+	err := self.KeyFacto.Check(key)
+	if nil != err {
+		return wrapError(err, "invalid key")
+	}
+
+	// lock storage
+	ts := key.T()
+	slot := &(self.slots[ts%numSlot])
+	slot.mut.Lock()
+	defer slot.mut.Unlock()
+
+	if ts != slot.t || nil == slot.store {
+		// slot contains expired data
+		slot.t = ts
+		slot.store = make(map[K]V)
+	}
+	slot.store[key] = data
+
+	return nil
+}
+
+// Save registers data in the MemStore using a new key.
+// Save returns the key indexing data.
+func (self *MemStore[K, V]) Save(data V) (K, error) {
+	var key K
+
+	// generates a key
+	key = self.KeyFacto.New()
+
+	// lock storage
+	ts := key.T()
+	slot := &(self.slots[ts%numSlot])
+	slot.mut.Lock()
+	defer slot.mut.Unlock()
+
+	if ts != slot.t || nil == slot.store {
+		// slot contains expired data
+		slot.t = ts
+		slot.store = make(map[K]V)
+	}
+	slot.store[key] = data
+
+	return key, nil
+}
---
+++ internal/session/memory_test.go
@@ -0,0 +1,143 @@
+package session
+
+import (
+	"testing"
+	"testing/synctest"
+	"time"
+)
+
+func TestMemStoreGet(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		lifetime := 32 * time.Second
+		store := getStore(t, lifetime)
+
+		// Get with a non registered key
+		// we use inner KeyFacto to generate this key so that it is valid...
+		k := store.KeyFacto.New()
+		_, found := store.Get(k)
+		if found {
+			t.Error("[0]: store.Get reports found on missing key")
+		}
+
+		// Add a value to the store
+		err := store.Set(k, "data")
+		if nil != err {
+			t.Fatalf("[1]: Failed store.Set, got error %v", err)
+		}
+
+		// Advance the clock just before expiration limit
+		time.Sleep(lifetime - 1*time.Nanosecond)
+		v, found := store.Get(k)
+		if !found {
+			t.Error("[2]: store.Get reports not found on existing key")
+		}
+		if v != "data" {
+			t.Errorf(`[3]: retrieved invalid v "%s" != "data"`, v)
+		}
+
+		// Pass the expiration limit
+		time.Sleep(2 * time.Nanosecond)
+		v, found = store.Get(k)
+		if found {
+			t.Error("[4]: store.Get reports found on expired key")
+		}
+
+	})
+}
+
+func TestMemStorePop(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		lifetime := 16 * time.Second
+		store := getStore(t, lifetime)
+
+		// Pop with a non valid key
+		k := Sid{}
+		_, found := store.Pop(k)
+		if found {
+			t.Fatal("store.Pop reports found on invalid key")
+		}
+
+		// Pop with a non registered key
+		// we use inner KeyFacto to generate this key so that it is valid...
+		k = store.KeyFacto.New()
+		_, found = store.Pop(k)
+		if found {
+			t.Errorf("store.Pop reports found on missing key")
+		}
+
+		// Save data in store
+		k, err := store.Save("something")
+		if nil != err {
+			t.Fatalf("store.Save failed, got error %v", err)
+		}
+
+		// try Pop, Pop, Set, Pop, Get, Set multiple times
+		deltas := []time.Duration{
+			4 * time.Second,
+			4 * time.Second,
+			4 * time.Second,
+			4*time.Second - 1*time.Nanosecond,
+		}
+		var s string
+		for step, delta := range deltas {
+			time.Sleep(delta) // advance the clock of delta
+			s, found = store.Pop(k)
+			if !found {
+				t.Fatalf("[%d] store.Pop reports not found with registered key", step)
+			}
+			if "something" != s {
+				t.Fatalf("[%d] store.Pop returned non expected value %s", step, s)
+			}
+			_, found = store.Pop(k)
+			if found {
+				t.Fatalf("[%d] store.Pop reports found with missing key", step)
+			}
+			err = store.Set(k, "some data")
+			if nil != err {
+				t.Fatalf("[%d] store.Set failed, got error %v", step, err)
+			}
+			s, found = store.Pop(k)
+			if !found {
+				t.Fatalf("[%d] store.Pop reports not found with registered key", step)
+			}
+			if "some data" != s {
+				t.Fatalf("[%d] store.Pop returned non expected value %s", step, s)
+			}
+			_, found = store.Get(k)
+			if found {
+				t.Fatalf("[%d] store.Get reports found with missing key", step)
+			}
+			err = store.Set(k, "something")
+			if nil != err {
+				t.Fatalf("[%d] store.Set failed, got error %v", step, err)
+			}
+
+		}
+		s, found = store.Get(k)
+		if !found {
+			t.Fatal("store.Get reports not found with registered key")
+		}
+		if "something" != s {
+			t.Fatalf("store.Pop returned non expected value %s", s)
+		}
+
+		// advance the clock to expire the key
+		time.Sleep(2 * time.Nanosecond)
+		_, found = store.Pop(k)
+		if found {
+			t.Fatal("store.Pop reports found with expired key")
+		}
+
+	})
+}
+
+func getStore(t *testing.T, lifetime time.Duration) *MemStore[Sid, string] {
+	sidfacto, err := NewSidFactory(lifetime)
+	if nil != err {
+		t.Fatalf("Failed NewSidFactory, got error %v", err)
+	}
+
+	ms := &MemStore[Sid, string]{KeyFacto: sidfacto}
+
+	return ms
+}
