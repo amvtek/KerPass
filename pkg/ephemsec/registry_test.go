7d550f7fb03edca0a58ca45a9c14cdc08b074fb9
amvtek <devel@amvtek.com>
2025-11-25 11:21:28 +0200

    Add EPHEMSEC registry to ease referencing instantations.

---
+++ pkg/ephemsec/registry_test.go
@@ -0,0 +1,16 @@
+package ephemsec_test
+
+import (
+	"testing"
+
+	"code.kerpass.org/golang/pkg/ephemsec"
+)
+
+func TestRegistry_GetScheme(t *testing.T) {
+	scm, err := ephemsec.GetScheme(ephemsec.SHA512_X25519_E1S1_T600B32P9)
+	if nil != err {
+		t.Fatalf("Failed scheme retrieval, got error %v", err)
+	}
+	t.Logf("scheme -> %s", scm.Name())
+	t.Logf("scheme.T -> %.2f s", scm.TimeWindow())
+}
