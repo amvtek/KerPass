23cfdc51302a4a9910a1745b6b879cbbacf8602f
amvtek <devel@amvtek.com>
2026-02-18 16:50:28 +0200

    Replace []byte by proper key type.

--- pkg/protocols/enroll/messages.go
+++ pkg/protocols/enroll/messages.go
@@ -1,20 +1,26 @@
 package enroll
 
+import (
+	"code.kerpass.org/golang/pkg/credentials"
+)
+
 // EnrollReq is sent by the CardAgent client to the KerPass server.
 // It is a plaintext that starts the EnrollProtocol.
 type EnrollReq struct {
-	RealmId []byte `json:"rid" cbor:"1,keyasint"` // Determine the Static Key used by the Server
-	Msg     []byte `json:"msg" cbor:"2,keyasint"`
+	RealmId credentials.RealmId `json:"rid" cbor:"1,keyasint"` // Determine the Static Key used by the Server
+	Msg     []byte              `json:"msg" cbor:"2,keyasint"`
 }
 
-func (self EnrollReq) Check() error {
-	rsz := len(self.RealmId)
-	if rsz < 32 || rsz > 64 {
-		return newError("invalid RealmId size, %d not in 32..64 range", rsz)
+func (self *EnrollReq) Check() error {
+	if nil == self {
+		return wrapError(ErrValidation, "nil EnrollReq")
+	}
+	if err := self.RealmId.Check(); nil != err {
+		return wrapError(err, "failed RealmId validation")
 	}
 	msz := len(self.Msg)
 	if msz < 32 {
-		return newError("invalid Noise Msg size, %d < 32", msz)
+		return wrapError(ErrValidation, "invalid Noise Msg size, %d < 32", msz)
 	}
 
 	return nil
@@ -22,13 +28,15 @@ func (self EnrollReq) Check() error {
 
 // EnrollAuthorization is sent by the CardAgent client to the KerPass server.
 type EnrollAuthorization struct {
-	AuthorizationId []byte `json:"aid" cbor:"1,keyasint"`
+	EnrollToken credentials.EnrollToken `json:"etk" cbor:"1,keyasint"`
 }
 
-func (self EnrollAuthorization) Check() error {
-	asz := len(self.AuthorizationId)
-	if asz < 32 {
-		return newError("invalid AuthorizationId size, %d < 32", asz)
+func (self *EnrollAuthorization) Check() error {
+	if nil == self {
+		return wrapError(ErrValidation, "nil EnrollAuthorization")
+	}
+	if err := self.EnrollToken.Check(); nil != err {
+		return wrapError(err, "failed EnrollToken validation")
 	}
 
 	return nil
@@ -37,17 +45,19 @@ func (self EnrollAuthorization) Check() error {
 // EnrollCardCreateResp is sent by the KerPass server to the CardAgent client.
 // It contains information that are necessary for creating the Card.
 type EnrollCardCreateResp struct {
-	IdToken []byte `json:"idt" cbor:"1,keyasint"`
-	UserId  string `json:"user_id,omitempty" cbor:"2,keyasint,omitempty"`
-	AppName string `json:"app_name" cbor:"3,keyasint"`
-	AppDesc string `json:"app_desc,omitempty" cbor:"4,keyasint,omitempty"`
-	AppLogo []byte `json:"app_logo,omitempty" cbor:"5,keyasint,omitempty"`
+	IdToken credentials.IdToken `json:"idt" cbor:"1,keyasint"`
+	UserId  string              `json:"user_id,omitempty" cbor:"2,keyasint,omitempty"`
+	AppName string              `json:"app_name" cbor:"3,keyasint"`
+	AppDesc string              `json:"app_desc,omitempty" cbor:"4,keyasint,omitempty"`
+	AppLogo []byte              `json:"app_logo,omitempty" cbor:"5,keyasint,omitempty"`
 }
 
-func (self EnrollCardCreateResp) Check() error {
-	csz := len(self.IdToken)
-	if csz < 32 {
-		return newError("Invalid IdToken size, %d < 32", csz)
+func (self *EnrollCardCreateResp) Check() error {
+	if nil == self {
+		return wrapError(ErrValidation, "nil EnrollCardCreateResp")
+	}
+	if err := self.IdToken.Check(); nil != err {
+		return wrapError(err, "failed IdToken validation")
 	}
 
 	return nil
