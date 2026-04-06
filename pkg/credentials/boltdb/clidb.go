49b955f158247d879aa357ae22e2eb19f560abfc
amvtek <devel@amvtek.com>
2026-04-06 12:08:02 +0300

    Implements ClientCredStore ifce changes.

--- pkg/credentials/boltdb/clidb.go
+++ pkg/credentials/boltdb/clidb.go
@@ -6,6 +6,7 @@ import (
 	"crypto"
 	"encoding/binary"
 	"math"
+	"strings"
 	"time"
 
 	"github.com/fxamacker/cbor/v2"
@@ -17,7 +18,6 @@ import (
 
 const (
 	connectTimeout = 5 * time.Second
-	maxCardId      = 0xFFFF_FFFF
 	hashAlgo       = crypto.BLAKE2s_256
 )
 
@@ -38,7 +38,9 @@ func New(dbpath string) (credentials.ClientCredStore, error) {
 
 	err = db.Update(func(tx *bolt.Tx) error {
 		var err error
-		for _, bucketname := range []string{"cardTbl", "realmIdx", "tokenIdx"} {
+
+		// create db buckets
+		for _, bucketname := range []string{"cardTbl", "cardTknIdx", "cardRlmIdx", "realmTbl", "realmIdx"} {
 			_, err = tx.CreateBucketIfNotExists([]byte(bucketname))
 			if nil != err {
 				return wrapError(err, "failed %s bucket creation", bucketname)
@@ -55,18 +57,12 @@ func New(dbpath string) (credentials.ClientCredStore, error) {
 
 }
 
-// SaveCard saves card in the cliCredStore.
+// CreateCard saves card in the cliCredStore.
 // It errors if the card could not be saved.
-func (self cliCredStore) SaveCard(card *credentials.Card) error {
+func (self cliCredStore) CreateCard(card *credentials.Card) error {
 	err := card.Check()
 	if nil != err {
-		return wrapError(err, "card is invalid")
-	}
-
-	// marshal card data using cbor
-	srzcard, err := cbor.Marshal(card)
-	if nil != err {
-		return wrapError(err, "failed cbor.Marshal(card)")
+		return wrapError(err, "invalid card")
 	}
 
 	db, err := bolt.Open(self.dbpath, 0600, &bolt.Options{Timeout: connectTimeout})
@@ -83,17 +79,51 @@ func (self cliCredStore) SaveCard(card *credentials.Card) error {
 			return wrapError(err, "failed loadSchema")
 		}
 
-		var curcard credentials.Card
+		// set realm
+		// note that existing realm are updated each time a new card is added.
+		var rlm credentials.Realm
+		err = card.RealmExport(&rlm)
+		if nil != err {
+			return wrapError(err, "failed exporting Realm")
+		}
+		srzrlm, err := cbor.Marshal(rlm)
+		if nil != err {
+			return wrapError(err, "failed cbor.Marshal of Realm")
+		}
+		rId := sch.realmIdx.Get(rlm.RealmId)
+		if nil == rId {
+			if sch.realmTbl.Sequence() >= math.MaxInt {
+				return wrapError(ErrSeqOverflow, "too many Realm")
+			}
+			uId, err := sch.realmTbl.NextSequence()
+			if nil != err {
+				return wrapError(err, "failed realmTbl.NextSequence")
+			}
+			rId = byteId(int(uId))
+			err = sch.realmIdx.Put(rlm.RealmId, rId)
+			if nil != err {
+				return wrapError(err, "failed updating realmIdx")
+			}
+		}
+		err = sch.realmTbl.Put(rId, srzrlm)
+		if nil != err {
+			return wrapError(err, "failed saving realm in realmTbl bucket")
+		}
+
+		// save new card
+		// note that cards are "immutable"
+		// existing cards are not changed
+		var curcard credentials.ClientCard
 		var found bool
 		if card.ID > 0 {
 
 			// card should be present in the store
-			found, err = sch.loadById(card.ID, &curcard)
+			found, err = sch.loadCardById(card.ID, &curcard)
 			if nil != err {
 				return wrapError(err, "failed loading existing card")
 			}
 			if !found {
-				return newError("card has invalid ID, not in store")
+				return wrapError(ErrNotFound, "failed loading existing card")
 			}
 
 			// check that RealmId & IdToken are the same
@@ -103,9 +133,11 @@ func (self cliCredStore) SaveCard(card *credentials.Card) error {
 					"forbidden operation, attend to change RealmId or IdToken of existing card",
 				)
 			}
+
+			return nil // curcard left unchanged
 		} else {
 			// see if a card with same IdToken exists
-			found, err := sch.loadByToken(card.IdToken, &curcard)
+			found, err := sch.loadCardByKey(card.IdToken, &curcard)
 			if nil != err {
 				return wrapError(err, "failed loading existing card")
 			}
@@ -117,44 +149,57 @@ func (self cliCredStore) SaveCard(card *credentials.Card) error {
 					)
 				}
 				card.ID = curcard.ID
-			} else {
-				// generates card ID
-				if sch.cardTbl.Sequence() >= maxCardId {
-					return newError("too many card")
-				}
-				nId, err := sch.cardTbl.NextSequence()
-				if nil != err {
-					return wrapError(err, "failed generating card ID")
-				}
-				card.ID = int(nId)
+
+				return nil // curcard left unchanged
 			}
 		}
 
+		// extract ClientCard
+		err = card.ClientExport(&curcard)
+		if nil != err {
+			return wrapError(err, "failed exporting ClientCard")
+		}
+
+		// generates card ID
+		if sch.cardTbl.Sequence() >= math.MaxInt {
+			return wrapError(ErrValidation, "too many card")
+		}
+		nId, err := sch.cardTbl.NextSequence()
+		if nil != err {
+			return wrapError(err, "failed generating card ID")
+		}
+		curcard.ID = int(nId)
+		card.ID = curcard.ID
+
 		csk := cardStoreKeys{}
-		readStoreKeys(card, &csk)
+		readStoreKeys(&curcard, &csk)
 
-		// store the srzcard
-		err = sch.cardTbl.Put(csk.cardKey, srzcard)
+		// store the ClientCard
+		srzcard, err := cbor.Marshal(curcard)
 		if nil != err {
-			return wrapError(err, "failed storing card in bucket")
+			return wrapError(err, "failed cbor.Marshal of ClientCard")
+		}
+		err = sch.cardTbl.Put(csk.cardId, srzcard)
+		if nil != err {
+			return wrapError(err, "failed storing card in cardTbl bucket")
 		}
 
-		// add entry in realmIdx
-		err = sch.realmIdx.Put(csk.realmKey, csk.cardKey)
+		// add entry in cardRlmIdx
+		err = sch.cardRlmIdx.Put(csk.realmKey, csk.cardId)
 		if nil != err {
-			return wrapError(err, "failed updating the realmIdx bucket")
+			return wrapError(err, "failed updating the cardRlmIdx bucket")
 		}
 
-		// add entry in tokenIdx
-		err = sch.tokenIdx.Put(csk.tokenKey, csk.cardKey)
+		// add entry in cardTknIdx
+		err = sch.cardTknIdx.Put(csk.tokenKey, csk.cardId)
 		if nil != err {
-			return wrapError(err, "failed updating the tokenIdx bucket")
+			return wrapError(err, "failed updating the cardTknIdx bucket")
 		}
 
 		return nil
 	})
 
-	return wrapError(err, "failed db.Update") // nil if err is nil
+	return err
 }
 
 // RemoveCard removes the Card with cId ID from the cliCredStore.
@@ -169,14 +214,14 @@ func (self cliCredStore) RemoveCard(cId int) (bool, error) {
 	var removed bool
 	err = db.Update(func(tx *bolt.Tx) error {
 		var err error
-		var card credentials.Card
+		var card credentials.ClientCard
 
 		sch, err := loadSchema(tx)
 		if nil != err {
 			return wrapError(err, "failed loading schema")
 		}
 
-		found, err := sch.loadById(cId, &card)
+		found, err := sch.loadCardById(cId, &card)
 		if nil != err {
 			return wrapError(err, "failed accessing existing card")
 		}
@@ -185,23 +230,24 @@ func (self cliCredStore) RemoveCard(cId int) (bool, error) {
 		}
 
 		csk := cardStoreKeys{}
+		card.ID = cId
 		readStoreKeys(&card, &csk)
 
-		err = sch.cardTbl.Delete(csk.cardKey)
+		err = sch.cardTbl.Delete(csk.cardId)
 		if nil != err {
 			// unlikely as cardTbl is writable
 			return err
 		}
 
-		err = sch.realmIdx.Delete(csk.realmKey)
+		err = sch.cardRlmIdx.Delete(csk.realmKey)
 		if nil != err {
-			// unlikely as realmIdx is writable
+			// unlikely as cardRlmIdx is writable
 			return err
 		}
 
-		err = sch.tokenIdx.Delete(csk.tokenKey)
+		err = sch.cardTknIdx.Delete(csk.tokenKey)
 		if nil != err {
-			// unlikely as tokenIdx is writable
+			// unlikely as cardTknIdx is writable
 			return err
 		}
 
@@ -210,19 +256,66 @@ func (self cliCredStore) RemoveCard(cId int) (bool, error) {
 		return nil
 	})
 
-	return removed, wrapError(err, "failed db.Update")
+	return removed, err
 }
 
-// LoadById loads the Card with ID cid into dst.
-// It returns true if the Card was found and successfully loaded.
-func (self cliCredStore) LoadById(cid int, dst *credentials.Card) (bool, error) {
+// SetCardLabel assign a new label to the card with cId ID.
+// It errors if the label could not be assigned.
+func (self cliCredStore) SetCardLabel(cId int, lbl string) error {
+	lbl = strings.TrimSpace(lbl)
+	if "" == lbl {
+		return wrapError(ErrValidation, "empty label")
+	}
+
 	db, err := bolt.Open(self.dbpath, 0600, &bolt.Options{Timeout: connectTimeout})
 	if nil != err {
-		return false, wrapError(err, "failed connecting to the database")
+		return wrapError(err, "failed connecting to the database")
 	}
 	defer db.Close()
 
-	var loaded bool
+	err = db.Update(func(tx *bolt.Tx) error {
+		var err error
+
+		sch, err := loadSchema(tx)
+		if nil != err {
+			return wrapError(err, "failed loading schema")
+		}
+
+		var card credentials.ClientCard
+		found, err := sch.loadCardById(cId, &card)
+		if nil != err {
+			return wrapError(err, "failed loading card")
+		}
+		if !found {
+			return wrapError(ErrNotFound, "missing card")
+		}
+		card.ID = cId
+		card.Label = lbl
+		srzcard, err := cbor.Marshal(card)
+		if nil != err {
+			return wrapError(err, "failed cbor.Marshal of ClientCard")
+		}
+		err = sch.cardTbl.Put(byteId(cId), srzcard)
+		if nil != err {
+			return wrapError(err, "failed storing card in cardTbl bucket")
+		}
+
+		return nil
+
+	})
+
+	return err
+}
+
+// LoadCard copies ClientCard with ID cId into dst.
+// It errors if the ClientCard could not be copied.
+func (self cliCredStore) LoadCard(cId int, dst *credentials.ClientCard) error {
+	db, err := bolt.Open(self.dbpath, 0600, &bolt.Options{Timeout: connectTimeout})
+	if nil != err {
+		return wrapError(err, "failed connecting to the database")
+	}
+	defer db.Close()
+
 	err = db.View(func(tx *bolt.Tx) error {
 		var err error
 
@@ -231,21 +324,55 @@ func (self cliCredStore) LoadById(cid int, dst *credentials.Card) (bool, error) 
 			return wrapError(err, "failed loading schema")
 		}
 
-		found, err := sch.loadById(cid, dst)
+		found, err := sch.loadCardById(cId, dst)
 		if nil != err {
 			return wrapError(err, "failed loading card")
 		}
-
-		loaded = found
+		if !found {
+			return wrapError(ErrNotFound, "missing card")
+		}
 
 		return nil
 	})
 
-	return loaded, err
+	return err
+}
+
+// LoadRealm copies the Realm keyed by rId into dst.
+// It errors if the Realm could not be copied
+func (self cliCredStore) LoadRealm(rId int, dst *credentials.Realm) error {
+	db, err := bolt.Open(self.dbpath, 0600, &bolt.Options{Timeout: connectTimeout})
+	if nil != err {
+		return wrapError(err, "failed connecting to the database")
+	}
+	defer db.Close()
+
+	err = db.View(func(tx *bolt.Tx) error {
+		var err error
+
+		sch, err := loadSchema(tx)
+		if nil != err {
+			return wrapError(err, "failed loading schema")
+		}
+
+		srzrlm := sch.realmTbl.Get(byteId(rId))
+		if nil == srzrlm {
+			return wrapError(ErrNotFound, "missing realm")
+		}
+		err = cbor.Unmarshal(srzrlm, dst)
+
+		return wrapError(err, "failed unmarshaling realm") // nil if err is nil
+	})
+
+	return err
 }
 
 // ListInfo returns a list of CardInfo that matches qry.
 func (self cliCredStore) ListInfo(qry credentials.CardQuery) ([]credentials.CardInfo, error) {
+	err := qry.Check()
+	if nil != err {
+		return nil, wrapError(err, "failed qry.Check")
+	}
 	db, err := bolt.Open(self.dbpath, 0600, &bolt.Options{Timeout: connectTimeout})
 	if nil != err {
 		return nil, wrapError(err, "failed connecting to the database")
@@ -255,30 +382,30 @@ func (self cliCredStore) ListInfo(qry credentials.CardQuery) ([]credentials.Card
 	rk := rangeKeys{}
 	readRangeKeys(qry, &rk)
 
-	// uses 16 as default qry.Limit
-	if 0 == qry.Limit {
-		qry.Limit = 16
-	}
-
-	cardlist := make([]credentials.CardInfo, 0, 4)
+	var infos []credentials.CardInfo
 	err = db.View(func(tx *bolt.Tx) error {
 		sch, err := loadSchema(tx)
 		if nil != err {
 			return wrapError(err, "failed loading schema")
 		}
 
+		rlmCache := make(map[int]credentials.Realm)
+
 		var srzcard []byte
 		var c *bolt.Cursor
 		var isRealmKey bool
 		var uId uint64
 		if len(qry.RealmId) > 0 {
-			c = sch.realmIdx.Cursor()
+			c = sch.cardRlmIdx.Cursor()
 			isRealmKey = true
 		} else {
 			c = sch.cardTbl.Cursor()
 			isRealmKey = false
 		}
-		for k, v := c.Seek(rk.minKey); k != nil && bytes.Compare(k, rk.maxKey) <= 0 && len(cardlist) < qry.Limit; k, v = c.Next() {
+		for k, v := c.Seek(rk.minKey); k != nil && bytes.Compare(k, rk.maxKey) <= 0; k, v = c.Next() {
+			if qry.Limit > 0 && len(infos) >= qry.Limit {
+				break
+			}
 			if isRealmKey {
 				srzcard = sch.cardTbl.Get(v)
 				uId = binary.BigEndian.Uint64(v)
@@ -289,24 +416,43 @@ func (self cliCredStore) ListInfo(qry credentials.CardQuery) ([]credentials.Card
 			if nil == srzcard {
 				continue
 			}
-			card := credentials.CardInfo{}
+
+			// load card
+			card := credentials.ClientCard{}
 			err = cbor.Unmarshal(srzcard, &card)
 			if nil != err {
 				return wrapError(err, "failed unmarshaling card")
 			}
 			if uId > math.MaxInt {
-				return newError("Invalid Card.ID")
+				return wrapError(ErrValidation, "Invalid ClientCard.ID")
 			} else {
 				card.ID = int(uId)
 			}
-			cardlist = append(cardlist, card)
+
+			// load card Realm
+			realm := credentials.Realm{}
+			rId, err := sch.loadRealmByKey(card.RealmId, rlmCache, &realm)
+			if nil != err {
+				return wrapError(err, "failed retrieving card realm")
+			}
+
+			// join card & realm into info
+			info := credentials.CardInfo{
+				ID:      card.ID,
+				RealmID: rId,
+				AppName: realm.AppName,
+				AppDesc: realm.AppDesc,
+				Label:   card.Label,
+			}
+
+			infos = append(infos, info)
 		}
 
 		return nil
 
 	})
 
-	return cardlist, err
+	return infos, err
 }
 
 // Size returns the number of Card in the ClientCredStore.
@@ -339,56 +485,100 @@ func (self cliCredStore) CardCount() int {
 
 // schema holds cliCredStore buckets reference
 type schema struct {
-	cardTbl  *bolt.Bucket
-	realmIdx *bolt.Bucket
-	tokenIdx *bolt.Bucket
+	cardTbl    *bolt.Bucket
+	cardTknIdx *bolt.Bucket
+	cardRlmIdx *bolt.Bucket
+	realmTbl   *bolt.Bucket
+	realmIdx   *bolt.Bucket
 }
 
 func loadSchema(tx *bolt.Tx) (schema, error) {
 	rv := schema{
-		cardTbl:  tx.Bucket([]byte("cardTbl")),
-		realmIdx: tx.Bucket([]byte("realmIdx")),
-		tokenIdx: tx.Bucket([]byte("tokenIdx")),
+		cardTbl:    tx.Bucket([]byte("cardTbl")),
+		cardTknIdx: tx.Bucket([]byte("cardTknIdx")),
+		cardRlmIdx: tx.Bucket([]byte("cardRlmIdx")),
+		realmTbl:   tx.Bucket([]byte("realmTbl")),
+		realmIdx:   tx.Bucket([]byte("realmIdx")),
 	}
 	var err error
-	if nil == rv.cardTbl || nil == rv.realmIdx || nil == rv.tokenIdx {
+	if nil == rv.cardTbl || nil == rv.cardTknIdx || nil == rv.cardRlmIdx || nil == rv.realmTbl || nil == rv.realmIdx {
 		err = newError("1 or more bucket is missing")
 	}
 
 	return rv, err
 }
 
-func (self schema) loadById(cId int, dst *credentials.Card) (bool, error) {
+func (self schema) loadCardById(cId int, dst *credentials.ClientCard) (bool, error) {
 	srzcard := self.cardTbl.Get(byteId(cId))
 	if nil == srzcard {
 		return false, nil
 	}
 
 	err := cbor.Unmarshal(srzcard, dst)
-	if nil == err {
-		dst.ID = cId
+	if nil != err {
+		return false, wrapError(err, "failed unmarshalling ClientCard")
 	}
+	dst.ID = cId
 
-	return true, err
+	return true, nil
 }
 
-func (self schema) loadByToken(tok []byte, dst *credentials.Card) (bool, error) {
-	cardId := self.tokenIdx.Get(hash(tok))
-	srzcard := self.cardTbl.Get(cardId)
-	if nil == srzcard {
+func (self schema) loadCardByKey(idtoken []byte, dst *credentials.ClientCard) (bool, error) {
+	srzId := self.cardTknIdx.Get(hash(idtoken))
+	if nil == srzId {
 		return false, nil
 	}
-	err := cbor.Unmarshal(srzcard, dst)
-	if nil == err {
-		cId := binary.BigEndian.Uint64(cardId)
-		if cId > math.MaxInt {
-			err = newError("invalid Card.ID")
-		} else {
-			dst.ID = int(cId)
-		}
+	srzcard := self.cardTbl.Get(srzId)
+	if nil == srzcard {
+		return false, wrapError(ErrNotFound, "missing ClientCard")
 	}
 
-	return true, err
+	err := cbor.Unmarshal(srzcard, dst)
+	if nil != err {
+		return false, wrapError(err, "failed unmarshalling ClientCard")
+	}
+	cId := binary.BigEndian.Uint64(srzId)
+	if cId > math.MaxInt {
+		return false, wrapError(ErrValidation, "cId > math.MaxInt")
+	}
+	dst.ID = int(cId)
+
+	return true, nil
+}
+
+// loadRealmByKey resolves realmid to a Realm, writing the result into dst.
+// It looks up realmid in realmCache first, then falls back to self.realmTbl.
+// Realms loaded from realmTbl are added to realmCache for subsequent calls.
+// It returns the integer ID of the Realm, which can be passed to LoadRealm.
+func (self schema) loadRealmByKey(realmid []byte, realmCache map[int]credentials.Realm, dst *credentials.Realm) (int, error) {
+	var rId int
+	srzId := self.realmIdx.Get(realmid)
+	if nil == srzId {
+		return rId, wrapError(ErrNotFound, "invalid realm RealmId")
+	}
+	uid := binary.BigEndian.Uint64(srzId)
+	if uid > math.MaxInt {
+		return rId, wrapError(ErrValidation, "invalid srzId")
+	}
+	rId = int(uid)
+	realm, found := realmCache[rId]
+	if found {
+		*dst = realm
+		return rId, nil
+	}
+	srzrlm := self.realmTbl.Get(srzId)
+	if nil == srzrlm {
+		return rId, wrapError(ErrNotFound, "missing srzrlm")
+	}
+	err := cbor.Unmarshal(srzrlm, &realm)
+	if nil != err {
+		return rId, wrapError(err, "failed unmarshaling realm")
+	}
+	realmCache[rId] = realm
+	*dst = realm
+
+	return rId, nil
+
 }
 
 // hash returns data digest
@@ -406,20 +596,28 @@ func hash(data []byte) []byte {
 
 // cardStoreKeys holds keys used by the cliCredStore to store & index Card informations.
 type cardStoreKeys struct {
-	cardKey  []byte // key Card in cardTbl bucket
-	realmKey []byte // key RealmId - Card in realmIdx bucket
-	tokenKey []byte // key IdToken - Card in tokenIdx bucket
+	// 8 bytes BigEndian encoding of card.ID.
+	// key serialized ClientCard in cardTbl bucket.
+	cardId []byte
+
+	// composite key [RealmId | cardId]
+	// key cardId in cardRlmIdx bucket.
+	realmKey []byte
+
+	// hash of card IdToken
+	// key cardId in cardTknIdx bucket.
+	tokenKey []byte
 }
 
-func readStoreKeys(card *credentials.Card, dst *cardStoreKeys) {
-	// cardKey
-	dst.cardKey = byteId(card.ID)
+func readStoreKeys(card *credentials.ClientCard, dst *cardStoreKeys) {
+	// cardId
+	dst.cardId = byteId(card.ID)
 
 	// realmKey
 	rsz := len(card.RealmId)
 	rk := make([]byte, rsz+8)
 	copy(rk, card.RealmId)
-	copy(rk[rsz:], dst.cardKey)
+	copy(rk[rsz:], dst.cardId)
 	dst.realmKey = rk
 
 	// tokenKey
@@ -446,7 +644,8 @@ func readRangeKeys(qry credentials.CardQuery, dst *rangeKeys) {
 	// minKey
 	minkey := make([]byte, psz+8)
 	copy(minkey, qry.RealmId)
-	binary.BigEndian.PutUint64(minkey[psz:], uint64(qry.MinId))
+	// qry.MinId is int, hence (uint64(qry.MinId) + 1) does not wrap to 0
+	binary.BigEndian.PutUint64(minkey[psz:], uint64(qry.MinId)+1)
 	dst.minKey = minkey
 
 	// maxKey
